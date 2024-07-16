71ed3aed3abe1109a81a9c3a9f4fb97688eed655
Paulo Felipe <paulo.felipe.pfsv@gmail.com>
2024-07-16 00:03:28 -0300

    Integração de criar atividades com a API

--- frontend/src/pages/trip-details/create-activity-modal.tsx
+++ frontend/src/pages/trip-details/create-activity-modal.tsx
@@ -1,18 +1,40 @@
 import { Calendar, Tag, X } from "lucide-react";
 import { Button } from "../../components/button";
+import { FormEvent } from "react";
+import { useParams } from "react-router-dom";
+import { api } from "../../libs/axios";
 
 interface CreateActivityModalProps {
   closeCreateActivityModal: () => void
 }
 
 export function CreateActivityModal({ closeCreateActivityModal }: CreateActivityModalProps) {
+
+  const { tripId } = useParams();
+
+  async function createActivity(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const data = new FormData(event.currentTarget);
+
+    const title = data.get('title')?.toString();
+    const occurs_at = data.get('occurs_at')?.toString();
+
+    await api.post(`/trips/${tripId}/activities`, {
+      title,
+      occurs_at
+    })
+
+    window.document.location.reload();
+  }
+
   return (
     <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
       <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
         <div className="space-y-2">
           <div className="flex items-center justify-between">
             <h2 className="text-lg font-semibold">
-              Confirmar criação de viagem
+              Cadastrar atividade
             </h2>
             <button type="button" onClick={closeCreateActivityModal}>
               <X className="size-5 text-zinc-400" />
@@ -23,7 +45,7 @@ export function CreateActivityModal({ closeCreateActivityModal }: CreateActivity
           </p>
         </div>
 
-        <form className="space-y-3">
+        <form onSubmit={createActivity} className="space-y-3">
           <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
             <Tag className="text-zinc-400 size-5" />
             <input
@@ -39,7 +61,7 @@ export function CreateActivityModal({ closeCreateActivityModal }: CreateActivity
               <Calendar className="text-zinc-400 size-5" />
               <input
                 type="datetime-local"
-                name="occoursAt"
+                name="occurs_at"
                 placeholder="Data e horário da atividade"
                 className="bg-transparent tex-lg placeholder-zinc-400 outline-none flex-1"
               />
