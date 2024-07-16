6728fdeb272ab353f49623c2b82162f7b067d6b9
Paulo Felipe <paulo.felipe.pfsv@gmail.com>
2024-07-15 22:08:04 -0300

    Correção de bug do return do tripId

--- backend/src/routes/create-trip.ts
+++ backend/src/routes/create-trip.ts
@@ -100,7 +100,7 @@ export async function createTrip(app: FastifyInstance) {
 
    console.log(nodemailer.getTestMessageUrl(message));
 
-   return { tripID: trip.id };
+   return { tripId: trip.id };
   }
  );
 }
