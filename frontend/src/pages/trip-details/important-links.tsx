6a412fb5313266e6680581d27d161ce5756d7089
Paulo Felipe <paulo.felipe.pfsv@gmail.com>
2024-07-18 15:14:07 -0300

    Integração com a busca dos links

--- frontend/src/pages/trip-details/important-links.tsx
+++ frontend/src/pages/trip-details/important-links.tsx
@@ -1,13 +1,25 @@
 import { Link2, Plus } from "lucide-react";
 import { Button } from "../../components/button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CreateLinkModal } from "./create-link-modal";
+import { useParams } from "react-router-dom";
+import { api } from "../../libs/axios";
 
-
+interface Link {
+  id: string;
+  title: string;
+  url: string;
+}
 
 export function ImportantLinks() {
 
-  const [isCreateLinkModalOpen, setIsCreateLinkModalOpen] = useState(false)
+  const [isCreateLinkModalOpen, setIsCreateLinkModalOpen] = useState(false);
+  const { tripId } = useParams();
+  const [links, setLinks] = useState<Link[]>([]);
+
+  useEffect(() => {
+    api.get(`/trips/${tripId}/links`).then(response => setLinks(response.data.links));
+  }, [tripId]);
 
   function openCreateLinkModal() {
     setIsCreateLinkModalOpen(true)
@@ -20,27 +32,21 @@ export function ImportantLinks() {
     <div className="space-y-6">
       <h2 className="font-semibold text-xl">Links importantes</h2>
       <div className="space-y-5">
-        <div className="flex items-center justify-between gap-4">
-          <div className="space-y-1.5">
-            <span className="block font-medium text-zinc-100">Reserva do AirBnB</span>
-            <a href="#" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">
-              https://www.airbnb.com.br/rooms/104700011213162378162387123
-            </a>
-          </div>
-          <Link2 className="size-5 text-zinc-400 shrink-0" />
-        </div>
-
-        <div className="flex items-center justify-between gap-4">
-          <div className="space-y-1.5">
-            <span className="block font-medium text-zinc-100">Reserva do AirBnB</span>
-            <a href="#" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">
-              https://www.airbnb.com.br/rooms/104700011213162378162387123
-            </a>
-          </div>
-          <Link2 className="size-5 text-zinc-400 shrink-0" />
-        </div>
+        {links.map(link => {
+          return (
+            <div key={link.id} className="flex items-center justify-between gap-4">
+              <div className="space-y-1.5">
+                <span className="block font-medium text-zinc-100">{link.title}</span>
+                <a href="#" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">
+                  {link.url}
+                </a>
+              </div>
+              <Link2 className="size-5 text-zinc-400 shrink-0" />
+            </div>
+          )
+        })}
       </div>
-      <Button onClick= {openCreateLinkModal} variant="secondary" size="full">
+      <Button onClick={openCreateLinkModal} variant="secondary" size="full">
         <Plus className="size-5" />
         Cadastrar novo link
       </Button>
