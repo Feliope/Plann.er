839d0a3478874d02287b496e2c46d1178cc1c716
Paulo Felipe <paulo.felipe.pfsv@gmail.com>
2024-07-18 13:59:16 -0300

    Adição do destino e das datas da viagem no modal de confirmação

--- frontend/src/pages/create-trip/confirm-trip-modal.tsx
+++ frontend/src/pages/create-trip/confirm-trip-modal.tsx
@@ -1,20 +1,31 @@
 import { Mail, User, X } from "lucide-react";
 import { FormEvent } from "react";
 import { Button } from "../../components/button";
+import { DateRange } from "react-day-picker";
+import { format } from "date-fns"
 
 interface ConfirmTripModalProps {
   closeConfirmTripModal: () => void;
   createTrip: (event: FormEvent<HTMLFormElement>) => void;
   setOwnerName: (name: string) => void;
   setOwnerEmail: (email: string) => void;
+  destination: string;
+  eventStartAndEndDates: DateRange | undefined;
 }
 
 export function ConfirmTripModal({
   closeConfirmTripModal,
   createTrip,
   setOwnerName,
-  setOwnerEmail
+  setOwnerEmail,
+  destination,
+  eventStartAndEndDates,
 }: ConfirmTripModalProps) {
+
+  const displayedDate = eventStartAndEndDates && eventStartAndEndDates.from  && eventStartAndEndDates.to? 
+  format(eventStartAndEndDates.from, "d 'de' LLL").concat(' até ', format(eventStartAndEndDates.to, "d 'de' LLL"))
+  : null
+
   return (
     <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
       <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
@@ -30,11 +41,11 @@ export function ConfirmTripModal({
           <p className="text-sm text-zinc-400">
             Para concluir a criação da viagem para{" "}
             <span className="font-semibold text-zinc-100">
-              Florianópolis, Brasil
+              {destination}
             </span>{" "}
             nas datas de{" "}
             <span className="font-semibold text-zinc-100">
-              16 a 27 de Agosto de 2024
+              {displayedDate}
             </span>{" "}
             preencha seus dados abaixo:
           </p>
--- frontend/src/pages/create-trip/index.tsx
+++ frontend/src/pages/create-trip/index.tsx
@@ -152,6 +152,8 @@ export function CreateTripPage() {
           createTrip={createTrip}
           setOwnerName={setOwnerName}
           setOwnerEmail={setOwnerEmail}
+          destination={destination}
+          eventStartAndEndDates={eventStartAndEndDates}
         />
       )}
     </div>
